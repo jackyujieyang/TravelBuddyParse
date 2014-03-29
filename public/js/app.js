6ce79feb736fedd9158708626e324e42cebfd952
Jack Yang <jackyujieyang@gmail.com>
2014-03-29 18:15:18 -0400

    updated app.js to include Facebook login logic

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,27 @@ $(function(){
 	//Initialize Parse
 	Parse.initialize("ygllhC3rTkJ6tXIVmYSjeoyFX5f3qzgt6l1zHko6", "mnkxvrJwTcSkgxm492Aa41LoUlLuoKavX6vq403K");
 
+	//Initialize Facebook
+	window.fbAsyncInit = function() {
+	  // init the FB JS SDK
+	  Parse.FacebookUtils.init({
+	    appId      : '772451966098433', // Facebook App ID
+	    channelUrl : '//travelbuddydev.parseapp.com/channel.html', // Channel File
+	    status     : true, // check login status
+	    cookie     : true, // enable cookies to allow Parse to access the session
+	    xfbml      : true  // parse XFBML
+	  });
+	};
+
+	//Async load FB JS SDK
+	(function(d, debug){
+	  var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+	  if (d.getElementById(id)) {return;}
+	  js = d.createElement('script'); js.id = id; js.async = true;
+	  js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
+	  ref.parentNode.insertBefore(js, ref);
+	}(document, /*debug*/ false));
+
 	var ProfileView = Parse.View.extend({
 		events: {
 			"click #logout": "logout"
@@ -17,8 +38,14 @@ $(function(){
 			$(this.el).html(this.template);
 		},
 		logout: function() {
-			new LoginView();
-			delete self;
+			Parse.User.logOut();
+			if(!Parse.User.current()) {
+				new LogInView();
+				self.undelegateEvents();
+				delete self;
+			} else {
+				alert("This is a problem logging out!");
+			}
 		}
 	});
 
@@ -35,8 +62,23 @@ $(function(){
 			$(this.el).html(this.template);
 		},
 		login: function() {
-			new ProfileView();
-			delete self;
+			Parse.FacebookUtils.logIn("email", {
+  			success: function(user) {
+    			if (!user.existed()) {
+      			alert("User signed up and logged in through Facebook!");
+    			} else {
+      			alert("User logged in through Facebook!");
+    			}
+    			console.log("Current user is: " + Parse.User.current());
+    			new ProfileView();
+          self.undelegateEvents();
+          delete self;
+  			},
+  			error: function(user, error) {
+    			alert("User cancelled the Facebook login or did not fully authorize.");
+  			}
+			});
+			return false;
 		}
 	});
 
