79499917e3c34dc1ad4c1018f5ffb523e44d6e3e
Jack Yang <jackyujieyang@gmail.com>
2014-03-29 19:34:19 -0400

    updated app.js

--- public/js/app.js
+++ public/js/app.js
@@ -25,6 +25,26 @@ $(function(){
 	  ref.parentNode.insertBefore(js, ref);
 	}(document, /*debug*/ false));
 
+	/* 
+	 * EditProfileView
+	 * The user can change his name, email or top destination
+	 * in the form, and submit the form. Or he can clikc the 
+	 * cancel button to return to his profile View.
+	 */
+	var EditProfileView = Parse.View.extend({
+		return;
+	});
+
+	/*
+	 * ProfileView
+	 * The user can see his prfile picture, name, email, and
+	 * top destination. He can click edit button to edit go 
+	 * to the page where he can edit his basic information.
+	 * He can click on the edit button below his profile
+	 * picture to upload a new profile picture. Or he can 
+	 * click on the logout button to logout and return to
+	 * the LoginView.
+	 */
 	var ProfileView = Parse.View.extend({
 		events: {
 			"click #logout": "logout"
@@ -49,6 +69,33 @@ $(function(){
 		}
 	});
 
+	/* 
+	 * HomeView
+	 * The HomeView display a collection of matches
+	 * to the user. The user can click on any of the
+	 * record to see profiles of matched travlers.
+	 * He can also tab Profile on the Nav bar to go see
+	 * and edit his own profile.
+	 */
+	var HomeView = Parse.View.extend({
+		return;
+	});
+
+	/*
+	 * TopDestView
+	 * If the user is just signed up, he will come to this
+	 * page to enter his top destination. The system will
+	 * match travlers based on this information.
+	 */
+	var TopDestView = Parse.View.extend({
+		return;
+	});
+
+	/*
+	 * Login View
+	 * If user is not currently signed in, he comes to this
+	 * page first. The user can login with Facebook
+	 */
 	var LoginView = Parse.View.extend({
 		events: {
 			"click #login": "login"
@@ -66,8 +113,10 @@ $(function(){
   			success: function(user) {
     			if (!user.existed()) {
       			alert("User signed up and logged in through Facebook!");
+      			// Go to TopDestView
     			} else {
       			alert("User logged in through Facebook!");
+      			// Go to HomeView
     			}
     			console.log("Current user is: " + Parse.User.current());
     			new ProfileView();
