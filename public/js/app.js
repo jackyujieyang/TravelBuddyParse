09f0fda3a3e92a497ca0ad8945956c51734181d8
Jack Yang <jackyujieyang@gmail.com>
2014-03-29 17:50:42 -0400

    updated index.html and app.js

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,28 @@ $(function(){
 	//Initialize Parse
 	Parse.initialize("ygllhC3rTkJ6tXIVmYSjeoyFX5f3qzgt6l1zHko6", "mnkxvrJwTcSkgxm492Aa41LoUlLuoKavX6vq403K");
 
+	var ProfileView = Parse.View.extend({
+		events: {
+			"click #logout": "logout"
+		},
+		el: ".content",
+		template: _.template($('#profile-template').html()),
+		initialize: function() {
+			this.render();
+		},
+		render: function() {
+			$(this.el).html(this.template);
+		},
+		logout: function() {
+			new LoginView();
+			delete self;
+		}
+	});
+
 	var LoginView = Parse.View.extend({
+		events: {
+			"click #login": "login"
+		},
 		el: ".content",
 		template: _.template($('#login-template').html()),
 		initialize: function() {
@@ -12,6 +33,10 @@ $(function(){
 		},
 		render: function() {
 			$(this.el).html(this.template);
+		},
+		login: function() {
+			new ProfileView();
+			delete self;
 		}
 	});
 
